068a5e666eb7ab19d80954b9c3d6830eb40216dd
eslammoataz <eslammezo2323@gmail.com>
2024-01-28 17:19:26 +0200

    addind folder

---
+++ Sarvicny.Application/Services/Specifications/CriteriaSpecifications/CriteriaWithServicesSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Sarvicny.Domain.Entities;
+using Sarvicny.Domain.Specification;
+
+namespace Sarvicny.Application.Services.Specifications.NewFolder
+{
+    public class CriteriaWithServicesSpecification : BaseSpecifications<Criteria>
+    {
+        public CriteriaWithServicesSpecification()
+        {
+            Includes.Add(s => s.Services);
+
+        }
+        public CriteriaWithServicesSpecification(string criteriaId) : base(c => c.CriteriaID == criteriaId)
+        {
+            Includes.Add(s => s.Services);
+
+        }
+    }
+}
